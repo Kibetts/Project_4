002bdd69391012f39c3bb1ffd4af5e834eb787d0
Brian Kibet <kibetsbrian@gmail.com>
2023-06-13 12:07:15 +0300

    add alert

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+    var submitButton = document.querySelector("button");
+
+    submitButton.addEventListener("click", function() {
+      alert("Message sent!");
+    });
+  });
+  
